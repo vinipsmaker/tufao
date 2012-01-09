5694819a544b3c8db7c38647ab6d6d1c3b3f7fec
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2012-01-09 02:02:49 -0300

    Some performance imrpovement over HttpServerResponse

--- src/httpserverresponse.cpp
+++ src/httpserverresponse.cpp
@@ -20,6 +20,12 @@
 #include "priv/httpserverresponse.h"
 #include "priv/reasonphrase.h"
 
+static const char crlf[] = "\r\n";
+#define CRLF crlf, sizeof(crlf) - 1
+
+static const char lastChunk[] = "0\r\n";
+#define LAST_CHUNK lastChunk, sizeof(lastChunk) - 1
+
 namespace Tufao {
 
 HttpServerResponse::HttpServerResponse(QIODevice *device, Options options,
@@ -45,10 +51,13 @@ bool HttpServerResponse::writeContinue()
             || !priv->device)
         return false;
 
-    if (priv->options.testFlag(HTTP_1_1))
-        priv->device->write("HTTP/1.1 100 Continue\r\n\r\n");
-    else
-        priv->device->write("HTTP/1.0 100 Continue\r\n\r\n");
+    if (priv->options.testFlag(HTTP_1_1)) {
+        static const char chunk[] = "HTTP/1.1 100 Continue\r\n\r\n";
+        priv->device->write(chunk, sizeof(chunk) - 1);
+    } else {
+        static const char chunk[] = "HTTP/1.0 100 Continue\r\n\r\n";
+        priv->device->write(chunk, sizeof(chunk) - 1);
+    }
     return true;
 }
 
@@ -60,22 +69,25 @@ bool HttpServerResponse::writeHead(int statusCode,
             || !priv->device)
         return false;
 
-    if (priv->options.testFlag(HttpServerResponse::HTTP_1_0))
-        priv->device->write("HTTP/1.0 ");
-    else
-        priv->device->write("HTTP/1.1 ");
+    if (priv->options.testFlag(HttpServerResponse::HTTP_1_0)) {
+        static const char chunk[] = "HTTP/1.0 ";
+        priv->device->write(chunk, sizeof(chunk) - 1);
+    } else {
+        static const char chunk[] = "HTTP/1.1 ";
+        priv->device->write(chunk, sizeof(chunk) - 1);
+    }
 
     priv->device->write(QByteArray::number(statusCode));
-    priv->device->write(" ");
+    priv->device->write(" ", 1);
     priv->device->write(reasonPhrase);
-    priv->device->write("\r\n");
+    priv->device->write(CRLF);
 
     for (Headers::const_iterator i = headers.constBegin()
          ;i != headers.end();++i) {
         priv->device->write(i.key());
-        priv->device->write(": ");
+        priv->device->write(": ", 2);
         priv->device->write(i.value());
-        priv->device->write("\r\n");
+        priv->device->write(CRLF);
     }
     priv->formattingState = Priv::HEADERS;
     return true;
@@ -87,22 +99,25 @@ bool HttpServerResponse::writeHead(int statusCode, const Headers &headers)
             || !priv->device)
         return false;
 
-    if (priv->options.testFlag(HttpServerResponse::HTTP_1_0))
-        priv->device->write("HTTP/1.0 ");
-    else
-        priv->device->write("HTTP/1.1 ");
+    if (priv->options.testFlag(HttpServerResponse::HTTP_1_0)) {
+        static const char chunk[] = "HTTP/1.0 ";
+        priv->device->write(chunk, sizeof(chunk) - 1);
+    } else {
+        static const char chunk[] = "HTTP/1.1 ";
+        priv->device->write(chunk, sizeof(chunk) - 1);
+    }
 
     priv->device->write(QByteArray::number(statusCode));
-    priv->device->write(" ");
+    priv->device->write(" ", 1);
     priv->device->write(Priv::reasonPhrase(statusCode));
-    priv->device->write("\r\n");
+    priv->device->write(CRLF);
 
     for (Headers::const_iterator i = headers.constBegin()
          ;i != headers.end();++i) {
         priv->device->write(i.key());
-        priv->device->write(": ");
+        priv->device->write(": ", 2);
         priv->device->write(i.value());
-        priv->device->write("\r\n");
+        priv->device->write(CRLF);
     }
     priv->formattingState = Priv::HEADERS;
     return true;
@@ -114,15 +129,18 @@ bool HttpServerResponse::writeHead(int statusCode)
             || !priv->device)
         return false;
 
-    if (priv->options.testFlag(HttpServerResponse::HTTP_1_0))
-        priv->device->write("HTTP/1.0 ");
-    else
-        priv->device->write("HTTP/1.1 ");
+    if (priv->options.testFlag(HttpServerResponse::HTTP_1_0)) {
+        static const char chunk[] = "HTTP/1.0 ";
+        priv->device->write(chunk, sizeof(chunk) - 1);
+    } else {
+        static const char chunk[] = "HTTP/1.1 ";
+        priv->device->write(chunk, sizeof(chunk) - 1);
+    }
 
     priv->device->write(QByteArray::number(statusCode));
-    priv->device->write(" ");
+    priv->device->write(" ", 1);
     priv->device->write(Priv::reasonPhrase(statusCode));
-    priv->device->write("\r\n");
+    priv->device->write(CRLF);
     priv->formattingState = Priv::HEADERS;
     return true;
 }
@@ -141,29 +159,36 @@ bool HttpServerResponse::write(const QByteArray &chunk)
         return false;
     case Priv::HEADERS:
     {
-        if (priv->options.testFlag(HttpServerResponse::KEEP_ALIVE))
-            priv->headers.replace("Connection", "keep-alive");
-        else
-            priv->headers.replace("Connection", "close");
+        if (priv->options.testFlag(HttpServerResponse::KEEP_ALIVE)) {
+            static const char key[] = "Connection", value[] = "keep-alive";
+            priv->headers.replace(QByteArray::fromRawData(key, sizeof(key) - 1),
+                                  QByteArray::fromRawData(value,
+                                                          sizeof(value) - 1));
+        } else {
+            static const char key[] = "Connection", value[] = "close";
+            priv->headers.replace(QByteArray::fromRawData(key, sizeof(key) - 1),
+                                  QByteArray::fromRawData(value,
+                                                          sizeof(value) - 1));
+        }
         priv->headers.insert("Transfer-Encoding", "chunked");
 
         for (Headers::iterator i = priv->headers.begin()
              ;i != priv->headers.end();++i) {
             priv->device->write(i.key());
-            priv->device->write(": ");
+            priv->device->write(": ", 2);
             priv->device->write(i.value());
-            priv->device->write("\r\n");
+            priv->device->write(CRLF);
         }
-        priv->device->write("\r\n");
+        priv->device->write(CRLF);
 
         priv->formattingState = Priv::MESSAGE_BODY;
     }
     case Priv::MESSAGE_BODY:
     {
         priv->device->write(QByteArray::number(chunk.size(), 16));
-        priv->device->write("\r\n");
+        priv->device->write(CRLF);
         priv->device->write(chunk);
-        priv->device->write("\r\n");
+        priv->device->write(CRLF);
     }
     } // switch (priv->formattingState)
     return true;
@@ -187,9 +212,9 @@ bool HttpServerResponse::addTrailers(const Headers &headers)
         for (Headers::const_iterator i = headers.constBegin()
              ;i != headers.end();++i) {
             priv->device->write(i.key());
-            priv->device->write(": ");
+            priv->device->write(": ", 2);
             priv->device->write(i.value());
-            priv->device->write("\r\n");
+            priv->device->write(CRLF);
         }
     }
     } // switch (priv->formattingState)
@@ -208,14 +233,14 @@ bool HttpServerResponse::addTrailer(const QByteArray &headerName,
     case Priv::HEADERS:
         return false;
     case Priv::MESSAGE_BODY:
-        priv->device->write("0\r\n");
+        priv->device->write(LAST_CHUNK);
         priv->formattingState = Priv::TRAILERS;
     case Priv::TRAILERS:
     {
         priv->device->write(headerName);
-        priv->device->write(": ");
+        priv->device->write(": ", 2);
         priv->device->write(headerValue);
-        priv->device->write("\r\n");
+        priv->device->write(CRLF);
     }
     } // switch (priv->formattingState)
     return true;
@@ -230,24 +255,50 @@ bool HttpServerResponse::end(const QByteArray &chunk)
     case Priv::HEADERS:
     {
         if (priv->options.testFlag(HttpServerResponse::HTTP_1_1)) {
-            if (priv->options.testFlag(HttpServerResponse::KEEP_ALIVE))
-                priv->headers.replace("Connection", "keep-alive");
-            else
-                priv->headers.replace("Connection", "close");
-            priv->headers.insert("Transfer-Encoding", "chunked");
+            {
+                static const char key[] = "Connection";
+                if (priv->options.testFlag(HttpServerResponse::KEEP_ALIVE)) {
+                    static const char value[] = "keep-alive";
+                    priv->headers
+                            .replace(QByteArray::fromRawData(key,
+                                                             sizeof(key) - 1),
+                                     QByteArray::fromRawData(value,
+                                                             sizeof(value)
+                                                             - 1));
+                } else {
+                    static const char value[] = "close";
+                    priv->headers
+                            .replace(QByteArray::fromRawData(key,
+                                                             sizeof(key) - 1),
+                                     QByteArray::fromRawData(value,
+                                                             sizeof(value)
+                                                             - 1));
+                }
+            }
+            {
+                static const char key[] = "Transfer-Encoding",
+                        value[] = "chunked";
+                priv->headers.insert(QByteArray::fromRawData(key,
+                                                             sizeof(key) - 1),
+                                     QByteArray::fromRawData(value,
+                                                             sizeof(value)
+                                                             - 1));
+            }
         } else {
-            priv->headers.replace("Content-Length",
+            static const char key[] = "Content-Length";
+            priv->headers.replace(QByteArray::fromRawData(key,
+                                                          sizeof(key) - 1),
                                   QByteArray::number(chunk.size()));
         }
 
         for (Headers::iterator i = priv->headers.begin()
              ;i != priv->headers.end();++i) {
             priv->device->write(i.key());
-            priv->device->write(": ");
+            priv->device->write(": ", 2);
             priv->device->write(i.value());
-            priv->device->write("\r\n");
+            priv->device->write(CRLF);
         }
-        priv->device->write("\r\n");
+        priv->device->write(CRLF);
 
         priv->formattingState = Priv::MESSAGE_BODY;
     }
@@ -256,14 +307,14 @@ bool HttpServerResponse::end(const QByteArray &chunk)
         if (chunk.size()) {
             if (priv->options.testFlag(HttpServerResponse::HTTP_1_1)) {
                 priv->device->write(QByteArray::number(chunk.size(), 16));
-                priv->device->write("\r\n");
+                priv->device->write(CRLF);
             }
             priv->device->write(chunk);
             if (priv->options.testFlag(HttpServerResponse::HTTP_1_1))
-                priv->device->write("\r\n");
+                priv->device->write(CRLF);
         }
         if (priv->options.testFlag(HttpServerResponse::HTTP_1_1)) {
-            priv->device->write("0\r\n");
+            priv->device->write(LAST_CHUNK);
             priv->formattingState = Priv::TRAILERS;
         } else {
             priv->device->close();
@@ -274,7 +325,7 @@ bool HttpServerResponse::end(const QByteArray &chunk)
     }
     case Priv::TRAILERS:
     {
-        priv->device->write("\r\n");
+        priv->device->write(CRLF);
         if (!priv->options.testFlag(HttpServerResponse::KEEP_ALIVE))
             priv->device->close();
 
