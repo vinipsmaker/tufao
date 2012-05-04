ac64f6b23c7c7fda419e7e8f59e5bd1b56346ca1
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2012-05-04 15:42:59 -0300

    updated tufao-plugin's application template

--- tools/qtcreatorplugin/templates/data/application/main.cpp
+++ tools/qtcreatorplugin/templates/data/application/main.cpp
@@ -1,13 +1,6 @@
-// NOTE:
-// If you want HTTPS support, uncomment all code
-
 #include <QtCore/QCoreApplication>
-//#include <QtCore/QFile>
-//#include <QtNetwork/QSslCertificate>
-//#include <QtNetwork/QSslKey>
 
 #include <Tufao/HttpServer>
-//#include <Tufao/HttpsServer>
 
 #include <Tufao/HttpServerRequestRouter>
 #include <Tufao/HttpFileServer>
@@ -22,41 +15,36 @@ int main(int argc, char *argv[])
     // First we create a event loop and the server objects
     QCoreApplication a(argc, argv);
     Tufao::HttpServer server;
-//    Tufao::HttpsServer secureServer;
 
-//    {
-//        QFile keyFile(":/key.pem");
-//        key.open(QIODevice::ReadOnly);
-//        QSslKey key(&keyFile, QSsl::Rsa, QSsl::Pem, QSsl::PrivateKey,
-//                    "password");
-//        secureServer.setPrivateKey(key);
-
-//        QFile cert(":/cert.pem");
-//        cert.open(QIODevice::ReadOnly);
-//        secureServer.setLocalCertificate(QSslCertificate(&cert));
-//    }
-
-    // Then we create a request router and configure a request router
+    // Then we create a request router and configure a request router...
     Tufao::HttpServerRequestRouter router;
+
+    //   ...using some handlers
+
+    // to allow you change the running code at runtime through plugins
     Tufao::HttpPluginServer pluginServer("routes.conf");
+    // do a request to http://localhost:8080/reload to reload the plugins
+    // without stopping the server
     PluginReloader pluginReloader(&pluginServer);
+    // to server static files under public folder
     Tufao::HttpFileServer fileServer("public");
+    // to respond the remaining requests with a "404 - not found"
     NotFoundHandler handler404;
 
+    // you should also create handlers to:
+    //   - "/" (maybe redirecting to some other path)
+    //   - "/favicon.ico" (to serve the page's icon)
     router.map(QRegExp(""), &pluginServer)
             .map(QRegExp("^/reload$"), &pluginReloader)
             .map(QRegExp(""), &fileServer)
             .map(QRegExp(""), &handler404);
 
-    // We set the router as global request handler
+    // We set the router as the global request handler
     QObject::connect(&server, SIGNAL(requestReady(Tufao::HttpServerRequest*,Tufao::HttpServerResponse*)),
                      &router, SLOT(handleRequest(Tufao::HttpServerRequest*,Tufao::HttpServerResponse*)));
-//    QObject::connect(&secureServer, SIGNAL(requestReady(Tufao::HttpServerRequest*,Tufao::HttpServerResponse*)),
-//                     &router, SLOT(handleRequest(Tufao::HttpServerRequest*,Tufao::HttpServerResponse*)));
 
-    // Last, we run our servers
+    // Last, we run our server
     server.listen(QHostAddress::Any, 8080);
-//    secureServer.listen(QHostAddress::Any, 443);
 
     return a.exec();
 }
