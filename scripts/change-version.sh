e00ac6655d55c473db61796d7fd974c989d6dbb2
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2012-06-05 00:18:35 -0300

    Added more reminders to change-version script

--- scripts/change-version.sh
+++ scripts/change-version.sh
@@ -3,4 +3,4 @@
 sed -i -e "s/^PROJECT_NUMBER.*\$/PROJECT_NUMBER         = $1/g" ../Doxyfile
 sed -i -e "s/^VERSION.*\$/VERSION = $1/g" ../tufao.pro
 
-echo 'Edit "..src/tufao_global.h" file manually'
+echo 'Remember to edit "../src/tufao_global.h" and "../README" files manually'
