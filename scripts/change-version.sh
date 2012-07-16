189b5a2fd924947882f1813b115bf57202b0055c
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2012-07-16 00:59:24 -0300

    CMake support

--- scripts/change-version.sh
+++ scripts/change-version.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sed -i -e "s/^PROJECT_NUMBER.*\$/PROJECT_NUMBER         = $1/g" ../Doxyfile
-sed -i -e "s/^VERSION.*\$/VERSION = $1/g" ../tufao.pro
 
-echo 'Remember to edit "../src/tufao_global.h" and "../README" files manually'
+echo 'Remember to edit manually:'
+echo '"../src/tufao_global.h"'
+echo '"../README"'
+echo '"../CMakeLists.txt"'
