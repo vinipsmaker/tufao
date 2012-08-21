dd562ecf6517574c2186608c19af86f442f59e89
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2012-08-21 05:09:13 -0300

    Documented Session

--- src/session.h
+++ src/session.h
@@ -29,32 +29,65 @@
 namespace Tufao {
 
 /*!
-  \note
-  All member functions of this class are inlined and should add the minimum (if
-  any) of overhead.
-  */
+ * This class provides easier access to the session's properties. It uses C++
+ * features used in containers to provide a familiar interface, such as
+ * overloading the operator [].
+ *
+ * \note
+ * All member functions of this class are inlined and should add the minimum (if
+ * any) of overhead.
+ *
+ * \sa
+ * SessionStore
+ */
 class TUFAO_EXPORT Session
 {
 public:
+    /*!
+     * Provides a object that give less verbose access to a session property.
+     *
+     * \note
+     * You shouldn't create it directly, but use Session::operator [].
+     *
+     * \sa
+     * Session
+     */
     class PropertyWrapper
     {
     public:
+        /*!
+         * Constructs a new PropertyWrappe object.
+         *
+         * \note
+         * This object will use \p session in every operation and you should
+         * ensure that \p session isn't destructed before the PropertyWrapper
+         * object.
+         */
         PropertyWrapper(Session &session, const QByteArray &key) :
             session(session),
             key(key)
         {}
 
+        /*!
+         * Returns true if this property exists for this session.
+         */
         operator bool() const
         {
             return session.hasValue(key);
         }
 
+        /*!
+         * Assigns \p value to this property and returns a reference to itself.
+         */
         PropertyWrapper &operator =(const QVariant &value)
         {
             session.setValue(key, value);
             return *this;
         }
 
+        /*!
+         * Returns this property's value.
+         */
         QVariant operator ()()
         {
             return session.value(key);
@@ -65,6 +98,13 @@ public:
         const QByteArray key;
     };
 
+    /*!
+     * Constructs a new Session object.
+     *
+     * The object will use \p store, \p request and \p response in every
+     * operation and you should ensure these objects aren't destructed before
+     * this Session object.
+     */
     Session(SessionStore &store, const HttpServerRequest &request,
             HttpServerResponse &response) :
         store(store),
@@ -72,21 +112,44 @@ public:
         response(response)
     {}
 
+    /*!
+     * Returns true if the session has a property accessible through \p key.
+     *
+     * \sa
+     * SessionStore::hasProperty
+     */
     bool hasValue(const QByteArray &key) const
     {
         return store.hasProperty(request, response, key);
     }
 
+    /*!
+     * Returns the value of the property referenced by \p key, or a null
+     * QVariant if the property isn't found.
+     *
+     * \sa
+     * SessionStore::property
+     */
     QVariant value(const QByteArray &key) const
     {
         return store.property(request, response, key);
     }
 
+    /*!
+     * Sets the property's value referenced by \p key to \p value.
+     *
+     * \sa
+     * SessionStore::setProperty
+     */
     void setValue(const QByteArray &key, const QVariant &value)
     {
         store.setProperty(request, response, key, value);
     }
 
+    /*!
+     * Returns a PropertyWrapper that will remember the \p key used to
+     * manipulate the session property.
+     */
     PropertyWrapper operator [](const QByteArray &key)
     {
         return PropertyWrapper(*this, key);
