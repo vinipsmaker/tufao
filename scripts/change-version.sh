4b05ca6b30637d375a729f0ea3ec1bbf88e83655
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2012-08-21 22:53:55 -0300

    fix: change-version script wasn't updating all fields necessary from Doxyfile

--- scripts/change-version.sh
+++ scripts/change-version.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sed -i -e "s/^PROJECT_NUMBER.*\$/PROJECT_NUMBER         = $1/g" ../Doxyfile
+sed -i -e "s,^PROJECT_NUMBER.*\$,PROJECT_NUMBER         = $1,g" ../Doxyfile
+sed -i -e "s,^QHP_NAMESPACE^.*\$,QHP_NAMESPACE          = \"tufao.$1\",g" ../Doxyfile
+sed -i -e "s,^QHP_VIRTUAL_FOLDER^.*\$,QHP_VIRTUAL_FOLDER     = \"tufao-$1\",g" ../Doxyfile
 
 echo 'Remember to edit manually:'
 echo '"../src/tufao_global.h"'
