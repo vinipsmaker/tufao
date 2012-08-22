64b5a9d3ed0a38dc655e9c42db36ff2dc200a133
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2012-08-22 15:39:58 -0300

    Updated change-version script

--- scripts/change-version.sh
+++ scripts/change-version.sh
@@ -1,10 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-sed -i -e "s,^PROJECT_NUMBER.*\$,PROJECT_NUMBER         = $1,g" ../Doxyfile
-sed -i -e "s,^QHP_NAMESPACE^.*\$,QHP_NAMESPACE          = \"tufao.$1\",g" ../Doxyfile
-sed -i -e "s,^QHP_VIRTUAL_FOLDER^.*\$,QHP_VIRTUAL_FOLDER     = \"tufao-$1\",g" ../Doxyfile
+IFS='.' read MAJOR MINOR PATCH <<< "${1}"
 
-echo 'Remember to edit manually:'
-echo '"../src/tufao_global.h"'
-echo '"../README"'
-echo '"../CMakeLists.txt"'
+if [ -z "${MAJOR}" ]; then
+    echo "SYNTAX ERROR"
+    echo "Usage:"
+    echo "$0 \${VERSION_MAJOR}"
+    echo "$0 \${VERSION_MAJOR}.\${VERSION_MINOR}"
+    echo "$0 \${VERSION_MAJOR}.\${VERSION_MINOR}.\${VERSION_PATCH}"
+    exit 1
+fi
+
+if [ -z "${MINOR}" ]; then
+    MINOR='0'
+fi
+
+if [ -z "${PATCH}" ]; then
+    PATCH='0'
+fi
+
+PROJECT_ROOT="$(dirname $0)/.."
+PROJECT_CONF="${PROJECT_ROOT}/CMakeLists.txt"
+
+# Edit CMakeLists.txt
+sed -i -r -e "s,(TUFAO_VERSION_MAJOR +)[[:digit:]]+,\1${MAJOR},g" "${PROJECT_CONF}"
+sed -i -r -e "s,(TUFAO_VERSION_MINOR +)[[:digit:]]+,\1${MINOR},g" "${PROJECT_CONF}"
+sed -i -r -e "s,(TUFAO_VERSION_PATCH +)[[:digit:]]+,\1${PATCH},g" "${PROJECT_CONF}"
+
+# Edit CMakeLists.txt
+sed -i -r -e "s,(TUFAO_VERSION_MAJOR +)[[:digit:]]+,\1${MAJOR},g" \
+    "${PROJECT_ROOT}/src/tufao_global.h"
+sed -i -r -e "s,(TUFAO_VERSION_MINOR +)[[:digit:]]+,\1${MINOR},g" \
+    "${PROJECT_ROOT}/src/tufao_global.h"
+
+echo 'Remember to update the README file'
