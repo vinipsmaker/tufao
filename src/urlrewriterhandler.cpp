e136b1164004a32f6ea03ef749accf137e2d0980
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2013-03-21 01:10:36 -0300

    UrlRewriterHandler added

---
+++ src/urlrewriterhandler.cpp
@@ -0,0 +1,56 @@
+/*  This file is part of the Tufão project
+    Copyright (C) 2013 Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any
+    later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "priv/urlrewriterhandler.h"
+#include "httpserverrequest.h"
+
+namespace Tufao {
+
+UrlRewriterHandler::UrlRewriterHandler(const QUrl &url, QObject *parent) :
+    AbstractHttpServerRequestHandler(parent),
+    priv(new Priv(url))
+{
+}
+
+void UrlRewriterHandler::setUrl(const QUrl &url)
+{
+    priv->url = url;
+}
+
+QUrl UrlRewriterHandler::url() const
+{
+    return priv->url;
+}
+
+std::function<bool(HttpServerRequest&, HttpServerResponse&)>
+UrlRewriterHandler::handler(const QUrl &url)
+{
+    return [url](HttpServerRequest &request, HttpServerResponse&) {
+        request.setUrl(url);
+        return false;
+    };
+}
+
+bool UrlRewriterHandler::handleRequest(HttpServerRequest &request,
+                                       HttpServerResponse &)
+{
+    request.setUrl(priv->url);
+    return false;
+}
+
+} // namespace Tufao
---
+++ src/urlrewriterhandler.h
@@ -0,0 +1,101 @@
+/*
+  Copyright (c) 2013 Vinícius dos Santos Oliveira
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+  */
+
+#ifndef TUFAO_URLREWRITERHANDLER_H
+#define TUFAO_URLREWRITERHANDLER_H
+
+#include "abstracthttpserverrequesthandler.h"
+#include <QtCore/QUrl>
+
+namespace Tufao {
+
+/*!
+  A handler that responds with "Not found" to every request.
+
+  Its purpose is to avoid boilerplate code.
+
+  \since
+  0.6
+  */
+class TUFAO_EXPORT UrlRewriterHandler: public AbstractHttpServerRequestHandler
+{
+    Q_OBJECT
+public:
+    /*!
+      Constructs a UrlRewriterHandler object.
+
+      \p parent is passed to the QObject constructor.
+
+      \p url will be the new url.
+
+      \since
+      1.0
+      */
+    explicit UrlRewriterHandler(const QUrl &url, QObject *parent = 0);
+
+    /*!
+      Sets the URL that will replace the old URL to \p url.
+
+      \since
+      1.0
+    */
+    void setUrl(const QUrl &url);
+
+    /*!
+      Returns the url used to replace the old URLs.
+
+      \since
+      1.0
+    */
+    QUrl url() const;
+
+    /*!
+      Returns a handler that don't depends on another object. The purpose of
+      this alternative handler is to free you of the worry of maintain the
+      UrlRewriterHandler's object (lifetime) while the functor object is being
+      used.
+
+      \since
+      1.0
+     */
+    static std::function<bool(HttpServerRequest&, HttpServerResponse&)>
+    handler(const QUrl &url);
+
+public slots:
+    /*!
+      Changes the \p request url and returns false. This method doesn't do
+      anything else.
+
+      \since
+      1.0
+      */
+    bool handleRequest(Tufao::HttpServerRequest &request,
+                       Tufao::HttpServerResponse &response) override;
+
+private:
+    struct Priv;
+    Priv *priv;
+};
+
+} // namespace Tufao
+
+#endif // TUFAO_URLREWRITERHANDLER_H
