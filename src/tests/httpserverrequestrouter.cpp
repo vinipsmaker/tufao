aeb8d052a15474861c834c42efa2e759b9cd7ef3
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2013-05-21 04:48:19 -0300

    Added HttpServerRequestRouter tests

---
+++ src/tests/httpserverrequestrouter.cpp
@@ -0,0 +1,50 @@
+#include "httpserverrequestrouter.h"
+#include <QtTest/QTest>
+#include <QtCore/QSharedPointer>
+#include "../httpserverrequestrouter.h"
+
+using namespace Tufao;
+
+struct Helper
+{
+    Helper(bool &b): b(b) {}
+    ~Helper() {b = false;}
+    bool &b;
+};
+
+void HttpServerRequestRouterTest::mappings()
+{
+    constexpr auto size = 3;
+    bool active[size] = {true, true, true};
+
+    HttpServerRequestRouter httpServerRequestRouter;
+
+    for (int i = 0;i != size;++i) {
+        QSharedPointer<Helper> p{new Helper{active[i]}};
+        QCOMPARE(httpServerRequestRouter.map({
+                    QRegularExpression{},
+                    [p](HttpServerRequest&, HttpServerResponse&){return true;}
+                }), i);
+    }
+
+    QCOMPARE(active[0], true);
+    QCOMPARE(active[1], true);
+    QCOMPARE(active[2], true);
+
+    httpServerRequestRouter.unmap(1);
+    QCOMPARE(active[0], true);
+    QCOMPARE(active[1], false);
+    QCOMPARE(active[2], true);
+
+    httpServerRequestRouter.unmap(1);
+    QCOMPARE(active[0], true);
+    QCOMPARE(active[1], false);
+    QCOMPARE(active[2], false);
+
+    httpServerRequestRouter.unmap(0);
+    QCOMPARE(active[0], false);
+    QCOMPARE(active[1], false);
+    QCOMPARE(active[2], false);
+}
+
+QTEST_APPLESS_MAIN(HttpServerRequestRouterTest)
---
+++ src/tests/httpserverrequestrouter.h
@@ -0,0 +1,8 @@
+#include <QtCore/QObject>
+
+class HttpServerRequestRouterTest: public QObject
+{
+    Q_OBJECT
+private slots:
+    void mappings();
+};
