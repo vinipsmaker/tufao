d2b7739ca93b90732398efe6dcda8aa2d3bd4615
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2014-02-07 21:02:30 -0300

    Revert "Add support for debug stream operators in Headers"
    This reverts commit 7593aab7d9b1b698f08e08b3c0a1fdacbaad9ab0.
    
    This commit's metadata about the author was wrong causing commands like
    git blame show incorrect info. The next commit will fix my unfortunate
    mistake.

--- src/headers.h
+++ src/headers.h
@@ -68,8 +68,6 @@ struct Headers: public QMultiMap<IByteArray, QByteArray>
                                              = QDateTime());
 };
 
-TUFAO_EXPORT QDebug operator<<(QDebug dbg, const Headers &headers);
-
 } // namespace Tufao
 
 #endif // TUFAO_HEADERS_H
