vinipsmaker/tufao

commit a6fa289820d5b1828abd3a4f16c863cf3f851026

Consistancy changes.
Changed tabs to spaces.  Moved context data member of HttpServerRequest
to Priv.

src/httpserverrequest.cpp
@@ -25,8 +25,7 @@ const http_parser_settings HttpServerRequest::Priv::httpSettingsInstance
 
 HttpServerRequest::HttpServerRequest(QAbstractSocket &socket, QObject *parent) :
     QObject(parent),
-	 priv(new Priv(this, socket)),
-	 m_context("")
+	 priv(new Priv(this, socket))
 {
     connect(&socket, SIGNAL(readyRead()), this, SLOT(onReadyRead()));
     connect(&socket, SIGNAL(disconnected()), this, SIGNAL(close()));
@@ -395,12 +394,12 @@ int HttpServerRequest::Priv::on_message_complete(http_parser *parser)
 
 void HttpServerRequest::setContext(const QString context)
 {
-	m_context = context;
+	priv->context = context;
 }
 
 QString HttpServerRequest::context(void) const
 {
-	return m_context;
+	return priv->context;
 }
 
 } // namespace Tufao

CMakeLists.txt
@@ -15,6 +15,11 @@ find_package(Qt5Core REQUIRED)
 find_package(Qt5Network REQUIRED)
 find_package(CXX11 REQUIRED)
 
+IF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+  SET(APPLE 1)
+ENDIF()
+
+
 get_target_property(QMAKE_EXECUTABLE Qt5::qmake LOCATION)
 function(QUERY_QMAKE VAR RESULT)
     exec_program("${QMAKE_EXECUTABLE}" ARGS "-query ${VAR}" RETURN_VALUE return_code OUTPUT_VARIABLE output)
@@ -47,6 +52,9 @@ set(TUFAO_LIBRARY "tufao${TUFAO_VERSION_MAJOR}")
 
 # Build options
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXX11_FLAGS}")
+IF(APPLE)
+ SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++")
+ENDIF()
 
 # Install info
 set(includedir include/tufao-${TUFAO_VERSION_MAJOR})

src/classhandler.h
@@ -39,7 +39,7 @@ struct ClassHandlerPluginInfo
 	//! The comma separated list of authors, e.g. "Timothy Reaves".
 	QString authors;
 	//! The contact email or URL.
-	QString	contact;
+	QString    contact;
 	//! The HTML description of the plugin.
 	QString description;
 };
@@ -62,7 +62,7 @@ struct ClassHandlerPluginInfo
  *
  * <p>This is achieved by breaking the URL paths up into a parameter list, and then invoking a method on the subclass
  * and passing the parameters to that method. So a URL request like:
- *		<tt>/forum/read/forumName/someForumName/threadName/someThreadName/page/6</tt>
+ *        <tt>/forum/read/forumName/someForumName/threadName/someThreadName/page/6</tt>
  * would call a method Forum::read(QString forumName, QString threadName, int pageNumber) as
  * read("someForumName", "someThreadName", 6);. (please read below for a more accurate description).  This would take a
  * user to a forum named <em>someForumName</em>, to a topic named <em>someThreadName</em>, to the <em>6</em>th page of
@@ -111,10 +111,10 @@ struct ClassHandlerPluginInfo
  *
  * <p>In the above example, the actual declaration of the method would be:
  * <tt>void read(Tufao::HttpServerRequest & request,
- *				Tufao::HttpServerResponse & response,
- *				QString forumName,
- *				QString threadName,
- *				int pageNumber)</tt>.  The parameter names are used to match the path components in the URL.  This means
+ *               Tufao::HttpServerResponse & response,
+ *               QString forumName,
+ *               QString threadName,
+ *               int pageNumber)</tt>.  The parameter names are used to match the path components in the URL.  This means
  * that a URL will always have an <em>even number</em> of path components after the class name; half for the name of
  * the parameters, and the other half for the values.  If a value cannot be converted to the type of the parameter,
  * the request is not handled, and a message logged.  So the request would end up passing to the next registered

src/classhandlermanager.cpp
@@ -185,10 +185,10 @@ void ClassHandlerManager::addPluginLocation(const QString location)
 #pragma mark Private Methods
 /* ****************************************************************************************************************** */
 bool ClassHandlerManager::processRequest(HttpServerRequest & request,
-													  HttpServerResponse & response,
-													  const QString className,
-													  const QString methodName,
-													  const QHash<QString, QString> arguments)
+										 HttpServerResponse & response,
+										 const QString className,
+										 const QString methodName,
+										 const QHash<QString, QString> arguments)
 {
 	bool handled = false;
 	bool canHandle = true;
@@ -214,17 +214,17 @@ bool ClassHandlerManager::processRequest(HttpServerRequest & request,
 			if(variants[argumentIndex].canConvert(methodType)) {
 				variants[argumentIndex].convert(methodType);
 				argumentTable[argumentIndex] = QGenericArgument(variants[argumentIndex].typeName(),
-																				variants[argumentIndex].data());
+																variants[argumentIndex].data());
 				qDebug() << "Converted "
-							<< arguments.value(parameterName)
-							<< " to type "
-							<< QVariant::typeToName(methodType)
-							<< " index "
-							<< argumentIndex;
+						 << arguments.value(parameterName)
+						 << " to type "
+						 << QVariant::typeToName(methodType)
+						 << " index "
+						 << argumentIndex;
 			} else {
 				qWarning() << "Can not convert "
-							  << arguments.value(parameterName)
-							  << " to type " << QVariant::typeToName(methodType);
+						   << arguments.value(parameterName)
+						   << " to type " << QVariant::typeToName(methodType);
 			}
 			argumentIndex+=1;
 		}
@@ -232,18 +232,18 @@ bool ClassHandlerManager::processRequest(HttpServerRequest & request,
 			variants[3].convert(QMetaType::Int);
 			// Check & insert context if necessary
 			method.invoke(handler->handler,
-							  Qt::DirectConnection,
-							  argumentTable[0],
-							  argumentTable[1],
-							  argumentTable[2],
-							  argumentTable[3],
-							  argumentTable[4],
-							  argumentTable[5],
-							  argumentTable[6],
-							  argumentTable[7],
-							  argumentTable[8],
-							  argumentTable[9]
-							  );
+						  Qt::DirectConnection,
+						  argumentTable[0],
+						  argumentTable[1],
+						  argumentTable[2],
+						  argumentTable[3],
+						  argumentTable[4],
+						  argumentTable[5],
+						  argumentTable[6],
+						  argumentTable[7],
+						  argumentTable[8],
+						  argumentTable[9]
+						  );
 			handled = true;
 		}
 	} else {
@@ -295,8 +295,8 @@ void ClassHandlerManager::registerHandler(ClassHandler * handler)
 }
 
 int ClassHandlerManager::selectMethod(const QString className,
-												  const QString methodName,
-												  const QHash<QString, QString> arguments) const
+									  const QString methodName,
+									  const QHash<QString, QString> arguments) const
 {
 	int methodIndex = -1;
 	uint parameterHash = qHash(methodName);
@@ -328,11 +328,11 @@ bool ClassHandlerManager::handleRequest(Tufao::HttpServerRequest & request, Tufa
 	int minimumPathComponents = useContext ? 3 : 2;
 	if (pathComponents.length() < minimumPathComponents) {
 		qWarning() << "Request was dispatched to handler, but too few path components found.  The path components are"
-					  << pathComponents;
+				   << pathComponents;
 	} else if(pathComponents.length() > minimumPathComponents  + 16) {
 		// We also can not have too many arguments; 16 is max, as that is 8 argumetns plus request & response
 		qWarning() << "Request was dispatched to handler, but too many path components found.  The path components are"
-					  << pathComponents;
+				   << pathComponents;
 	} else {
 		if(!useContext || m_context == pathComponents[0]) {
 			// Add the context to the request

src/classhandlermanager.h
@@ -120,10 +120,10 @@ private:
 	void registerHandler(ClassHandler * handler);
 
 	bool processRequest(HttpServerRequest & request,
-							  HttpServerResponse & response,
-							  const QString className,
-							  const QString methodName,
-							  const QHash<QString, QString> arguments);
+						HttpServerResponse & response,
+						const QString className,
+						const QString methodName,
+						const QHash<QString, QString> arguments);
 
 	int selectMethod(const QString className, const QString methodName, const QHash<QString, QString> arguments) const;
 

src/httpserverrequest.h
@@ -54,7 +54,6 @@ enum class HttpVersion
 class TUFAO_EXPORT HttpServerRequest : public QObject
 {
     Q_OBJECT
-	 Q_PROPERTY(QString context READ context WRITE setContext)
 public:
 
     /*!
@@ -361,7 +360,6 @@ private:
 
     struct Priv;
     Priv *priv;
-	 QString m_context;
 
     friend struct Tufao::HttpServerRequest::Priv;
 };

src/priv/httpserverrequest.h
@@ -51,7 +51,8 @@ struct HttpServerRequest::Priv
         useTrailers(false),
         whatEmit(0),
         responseOptions(0),
-        timeout(0)
+		timeout(0),
+		context("")
     {
         http_parser_init(&parser, HTTP_REQUEST);
         timer.setSingleShot(true);
@@ -87,6 +88,7 @@ struct HttpServerRequest::Priv
 
     int timeout;
     QTimer timer;
+	QString context;
 
     static const http_parser_settings httpSettingsInstance;
 };

commit 3096233d85c67e7f577d15d3ce653f94041c389b

Fixing identation issue introduced in commit a6fa289820d5b1828abd3a4f16c863cf3f851026

src/httpserverrequest.cpp
@@ -25,7 +25,7 @@ const http_parser_settings HttpServerRequest::Priv::httpSettingsInstance
 
 HttpServerRequest::HttpServerRequest(QAbstractSocket &socket, QObject *parent) :
     QObject(parent),
-	 priv(new Priv(this, socket))
+    priv(new Priv(this, socket))
 {
     connect(&socket, SIGNAL(readyRead()), this, SLOT(onReadyRead()));
     connect(&socket, SIGNAL(disconnected()), this, SIGNAL(close()));