7a07e0e5746389a5761114cca7abc0cd646dd4b3
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2014-02-25 17:49:03 -0300

    Using standard-way to include C-headers in sample_plugin example

--- examples/cmake/sample_plugin/src/main.hpp
+++ examples/cmake/sample_plugin/src/main.hpp
@@ -24,7 +24,7 @@
 #define MAIN_HPP_
 
 #include <QDebug>
-#include <stdlib.h>
+#include <cstdlib>
 
 // A macro that prints a message and exits the application.
 #define printErrorAndExit(x, ...) qCritical(x, ##__VA_ARGS__);exit(1);
