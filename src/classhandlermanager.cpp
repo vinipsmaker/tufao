vinipsmaker/tufao

commit 827c9e84c3eac05ff0f448b2bc67be3cb397cf8e

Removing lazy evaluation from ClassHandlerManager's pluginLocations global variable.

src/classhandlermanager.cpp
@@ -43,7 +43,36 @@
 namespace Tufao {
 
 // Initialize static members.
-QStringList ClassHandlerManager::pluginLocations;
+QStringList ClassHandlerManager::pluginLocations = []() {
+    QStringList ret;
+
+    // Add standard locations to pluginLocations
+    // First, the typical app config dir's
+#ifdef Q_OS_WIN
+    // Code can be added here, but, for the time being, I'm leaving it empty.
+#elif defined(Q_OS_MAC)
+    ret.append(QDir::homePath() + "/Library/Application Support/Tufao");
+    ret.append("/Library/Application Support/Tufao");
+#else
+    ret.append(QDir::homePath() + "/.tufao");
+#endif
+
+    // The standard library locations
+    for (const QString &libraryPath: QCoreApplication::libraryPaths()) {
+        QDir testDir(libraryPath + QDir::separator() + "Tufao");
+        if(testDir.exists()){
+            ret.append(testDir.absolutePath());
+        }
+    }
+
+    // Finally, the install location
+    QFileInfo installDir(QCoreApplication::applicationDirPath());
+    if(installDir.isDir()) {
+        ret.append(installDir.absolutePath());
+    }
+
+    return ret;
+}();
 
 /* ************************************************************************** */
 /* Object lifecycle                                                           */
@@ -54,36 +83,6 @@ ClassHandlerManager::ClassHandlerManager(const QString &pluginID,
     QObject(parent),
     priv{new Priv{pluginID, urlNamespace}}
 {
-    // Set up the search locations
-    if(pluginLocations.isEmpty()) {
-        // Add standard locations to pluginLocations
-        // First, the typical app config dir's
-#ifdef Q_OS_WIN
-        // Code can be added here, but, for the time being, I'm leaving it empty.
-#elif defined(Q_OS_MAC)
-        pluginLocations.append(QDir::homePath() + "/Library/Application Support/Tufao");
-        pluginLocations.append("/Library/Application Support/Tufao");
-#else
-        pluginLocations.append(QDir::homePath() + "/.tufao");
-#endif
-
-        // The standard library locations
-        foreach (QString libraryPath, QCoreApplication::libraryPaths()) {
-            QDir testDir(libraryPath + QDir::separator() + "Tufao");
-            if(testDir.exists()){
-                pluginLocations.append(testDir.absolutePath());
-            }
-        }
-
-        // Finally, the install location
-        QFileInfo installDir(QCoreApplication::applicationDirPath());
-        if(installDir.isDir()) {
-            pluginLocations.append(installDir.absolutePath());
-        }
-
-        qDebug() << "pluginLocations:" << pluginLocations;
-    }
-
     // Now load the plugin of interest
     // First list all static plugins.
     foreach (QObject * pluginInterface, QPluginLoader::staticInstances()){

commit ad6e4bd7a158718db56458e8207938f8650d5490

Fixing bug introduced in commit 827c9e84c3eac05ff0f448b2bc67be3cb397cf8e.
QCoreApplication::libraryPath can only be retrieved at runtime,
after the QCoreApplication object was constructed.

I learnt how to combine ArchLinux's makepkg with local repo
cloning and I'll stop to push untested changes just to improve my
workflow, because know (with new knowledge), my workflow is
unaffected.

src/classhandlermanager.cpp
@@ -43,6 +43,11 @@
 namespace Tufao {
 
 // Initialize static members.
+
+/* \warning the variable is never fully initialized.
+ * `QCoreApplication::applicationDirPath()` (aka the install location), which
+ * can only be retrieved at runtime, after the QCoreApplication was constructed,
+ * is never present in this list. */
 QStringList ClassHandlerManager::pluginLocations = []() {
     QStringList ret;
 
@@ -63,12 +68,6 @@ QStringList ClassHandlerManager::pluginLocations = []() {
         if(testDir.exists()){
             ret.append(testDir.absolutePath());
         }
-    }
-
-    // Finally, the install location
-    QFileInfo installDir(QCoreApplication::applicationDirPath());
-    if(installDir.isDir()) {
-        ret.append(installDir.absolutePath());
     }
 
     return ret;
@@ -95,16 +94,26 @@ ClassHandlerManager::ClassHandlerManager(const QString &pluginID,
     // Then list dynamic libraries from the plugins/ directory
     QStringList contents;
     // retrieve a list of all dynamic libraries from the search paths
-    foreach (QString path, pluginLocations) {
-        QFileInfo thisPath(QDir(path).filePath("plugins"));
-        if(thisPath.isDir()){
-            QDir thisDir(thisPath.absoluteFilePath());
-            qDebug() << "Search " << thisPath.absolutePath() << " for plugins.";
-            foreach (const QString entry, thisDir.entryList()) {
-                if (QLibrary::isLibrary(entry))
-                    contents.append(thisDir.filePath(entry));
+    {
+        auto retrieveDynlib = [&contents](const QString &path) {
+            QFileInfo thisPath(QDir(path).filePath("plugins"));
+            if (thisPath.isDir()) {
+                QDir thisDir(thisPath.absoluteFilePath());
+                qDebug() << "Search " << thisPath.absolutePath()
+                         << " for plugins.";
+                for (const QString &entry: thisDir.entryList()) {
+                    if (QLibrary::isLibrary(entry))
+                        contents.append(thisDir.filePath(entry));
+                }
             }
-        }
+        };
+
+        for (const QString &path: pluginLocations)
+            retrieveDynlib(path);
+
+        QFileInfo installDir(QCoreApplication::applicationDirPath());
+        if (installDir.isDir())
+            retrieveDynlib(installDir.absolutePath());
     }
 
     // Check each dynamic library to see if it is a plugin