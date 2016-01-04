0a76ae49079b1065f0bf255bc5c0e5014aa5537a
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2016-01-03 23:27:42 -0300

    upgraded http-parser to version 2.6.0

--- src/httpserverrequest.cpp
+++ src/httpserverrequest.cpp
@@ -290,6 +290,10 @@ int HttpServerRequest::Priv::on_headers_complete(http_parser *parser)
             MT("PROPPATCH"),
             MT("SEARCH"),
             MT("UNLOCK"),
+            MT("BIND"),
+            MT("REBIND"),
+            MT("UNBIND"),
+            MT("ACL"),
             MT("REPORT"),
             MT("MKACTIVITY"),
             MT("CHECKOUT"),
@@ -300,7 +304,9 @@ int HttpServerRequest::Priv::on_headers_complete(http_parser *parser)
             MT("UNSUBSCRIBE"),
             MT("PATCH"),
             MT("PURGE"),
-            MT("MKCALENDAR")
+            MT("MKCALENDAR"),
+            MT("LINK"),
+            MT("UNLINK")
         };
 
         const MT &m = methods[parser->method];
