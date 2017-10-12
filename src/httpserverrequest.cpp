56e27d3b77d617ad1b4aea377f993592bc2c0d77
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2017-10-12 01:37:51 -0300

    Replace Boost.Http parser with Boost.Beast parser

--- src/httpserverrequest.cpp
+++ src/httpserverrequest.cpp
@@ -22,7 +22,7 @@ namespace Tufao {
 
 HttpServerRequest::HttpServerRequest(QAbstractSocket &socket, QObject *parent) :
     QObject(parent),
-    priv(new Priv(socket))
+    priv(new Priv(this, socket))
 {
     connect(&socket, SIGNAL(readyRead()), this, SLOT(onReadyRead()));
     connect(&socket, SIGNAL(disconnected()), this, SIGNAL(close()));
@@ -128,140 +128,27 @@ void HttpServerRequest::onReadyRead()
         priv->timer.start(priv->timeout);
 
     priv->buffer += priv->socket.readAll();
-    priv->parser.set_buffer(asio::buffer(priv->buffer.data(),
-                                         priv->buffer.size()));
 
     std::size_t nparsed = 0;
-    Priv::Signals whatEmit(0);
-    bool is_upgrade = false;
+    boost::system::error_code ec;
+    priv->whatEmit = Priv::Signals(0);
+    priv->is_upgrade = false;
 
     do {
-        priv->parser.next();
-        switch(priv->parser.code()) {
-        case http::token::code::error_insufficient_data:
-            continue;
-        case http::token::code::error_set_method:
-            qFatal("unreachable");
-            break;
-        case http::token::code::error_use_another_connection:
-            qFatal("unreachable");
-            break;
-        case http::token::code::error_invalid_data:
-        case http::token::code::error_no_host:
-        case http::token::code::error_invalid_content_length:
-        case http::token::code::error_content_length_overflow:
-        case http::token::code::error_invalid_transfer_encoding:
-        case http::token::code::error_chunk_size_overflow:
-            priv->socket.close();
-            return;
-        case http::token::code::skip:
-            break;
-        case http::token::code::method:
-            {
-                clearRequest();
-                priv->responseOptions = 0;
-                auto value = priv->parser.value<http::token::method>();
-                QByteArray method(value.data(), value.size());
-                priv->method = std::move(method);
-            }
-            break;
-        case http::token::code::request_target:
-            {
-                auto value = priv->parser.value<http::token::request_target>();
-                QByteArray url(value.data(), value.size());
-                priv->url = std::move(url);
-            }
-            break;
-        case http::token::code::version:
-            {
-                auto value = priv->parser.value<http::token::version>();
-                if (value == 0) {
-                    priv->httpVersion = HttpVersion::HTTP_1_0;
-                    priv->responseOptions |= HttpServerResponse::HTTP_1_0;
-                } else {
-                    priv->httpVersion = HttpVersion::HTTP_1_1;
-                    priv->responseOptions |= HttpServerResponse::HTTP_1_1;
-                }
-            }
-            break;
-        case http::token::code::status_code:
-            qFatal("unreachable");
-            break;
-        case http::token::code::reason_phrase:
-            qFatal("unreachable");
-            break;
-        case http::token::code::field_name:
-            {
-                auto value = priv->parser.value<http::token::field_name>();
-                priv->lastHeader = QByteArray(value.data(), value.size());
-            }
-            break;
-        case http::token::code::field_value:
-            {
-                auto value = priv->parser.value<http::token::field_value>();
-                QByteArray header(value.data(), value.size());
-                (priv->useTrailers ? priv->trailers : priv->headers)
-                    .insert(priv->lastHeader, std::move(header));
-                priv->lastHeader.clear();
-            }
-            break;
-        case http::token::code::end_of_headers:
-            {
-                auto it = priv->headers.find("connection");
-                bool close_found = false;
-                bool keep_alive_found = false;
-                for (;it != priv->headers.end();++it) {
-                    auto value = boost::string_ref(it->data(), it->size());
-                    http::header_value_any_of(value, [&](boost::string_ref v) {
-                        if (iequals(v, "close"))
-                            close_found = true;
+        std::array<asio::const_buffer, 1> buffers;
+        buffers[0]
+            = asio::buffer(priv->buffer.data(), priv->buffer.size()) + nparsed;
+        nparsed += priv->put(buffers, ec);
+    } while (!ec && !priv->whatEmit.testFlag(Priv::END));
 
-                        if (iequals(v, "keep-alive"))
-                            keep_alive_found = true;
+    if (ec && ec != http::error::need_more) {
+        priv->socket.close();
+        return;
+    }
 
-                        if (iequals(v, "upgrade"))
-                            is_upgrade = true;
-
-                        return false;
-                    });
-                    if (close_found)
-                        break;
-                }
-                if (!close_found
-                    && (priv->httpVersion == HttpVersion::HTTP_1_1
-                        || keep_alive_found)) {
-                    priv->responseOptions |= HttpServerResponse::KEEP_ALIVE;
-                }
-                whatEmit = Priv::READY;
-            }
-            break;
-        case http::token::code::body_chunk:
-            {
-                auto value = priv->parser.value<http::token::body_chunk>();
-                priv->body.append(asio::buffer_cast<const char*>(value),
-                                  asio::buffer_size(value));
-                whatEmit |= Priv::DATA;
-            }
-            break;
-        case http::token::code::end_of_body:
-            priv->useTrailers = true;
-            break;
-        case http::token::code::end_of_message:
-            priv->useTrailers = false;
-            priv->parser.set_buffer(asio::buffer(priv->buffer.data() + nparsed,
-                                                 priv->parser.token_size()));
-            whatEmit |= Priv::END;
-            disconnect(&priv->socket, SIGNAL(readyRead()),
-                       this, SLOT(onReadyRead()));
-            break;
-        }
-
-        nparsed += priv->parser.token_size();
-    } while(priv->parser.code() != http::token::code::error_insufficient_data
-            && priv->parser.code() != http::token::code::end_of_message);
     priv->buffer.remove(0, nparsed);
 
-    if (is_upgrade) {
+    if (priv->is_upgrade) {
         disconnect(&priv->socket, SIGNAL(readyRead()),
                    this, SLOT(onReadyRead()));
         disconnect(&priv->socket, SIGNAL(disconnected()),
@@ -272,6 +159,8 @@ void HttpServerRequest::onReadyRead()
         emit upgrade();
         return;
     }
+
+    Priv::Signals whatEmit(priv->whatEmit);
 
     if (whatEmit.testFlag(Priv::READY)) {
         whatEmit &= ~Priv::Signals(Priv::READY);
--- src/priv/httpserverrequest.h
+++ src/priv/httpserverrequest.h
@@ -20,6 +20,7 @@
 #define TUFAO_PRIV_HTTPSERVERREQUEST_H
 
 #include <boost/http/reader/request.hpp>
+#include <boost/beast/http/basic_parser.hpp>
 
 #include "../headers.h"
 #include "../httpserverrequest.h"
@@ -30,11 +31,11 @@
 
 namespace Tufao {
 
-namespace http = boost::http;
-namespace reader = http::reader;
+namespace http = boost::beast::http;
 namespace asio = boost::asio;
 
 struct HttpServerRequest::Priv
+    : public http::basic_parser<true, HttpServerRequest::Priv>
 {
     enum Signal
     {
@@ -44,7 +45,8 @@ struct HttpServerRequest::Priv
     };
     Q_DECLARE_FLAGS(Signals, Signal)
 
-    Priv(QAbstractSocket &socket) :
+    Priv(Tufao::HttpServerRequest *request, QAbstractSocket &socket) :
+        request(request),
         socket(socket),
         useTrailers(false),
         responseOptions(0),
@@ -53,10 +55,107 @@ struct HttpServerRequest::Priv
         timer.setSingleShot(true);
     }
 
+    void on_request_impl(http::verb, boost::string_view method_str,
+                         boost::string_view target, int version,
+                         boost::system::error_code&)
+    {
+        request->clearRequest();
+        responseOptions = 0;
+        QByteArray method(method_str.data(), method_str.size());
+        method = std::move(method);
+
+        QByteArray url(target.data(), target.size());
+        url = std::move(url);
+
+        if (version == 10) {
+            httpVersion = HttpVersion::HTTP_1_0;
+            responseOptions |= HttpServerResponse::HTTP_1_0;
+        } else {
+            httpVersion = HttpVersion::HTTP_1_1;
+            responseOptions |= HttpServerResponse::HTTP_1_1;
+        }
+    }
+
+    void on_field_impl(http::field, boost::string_view name,
+                       boost::string_view value, boost::system::error_code&)
+    {
+        QByteArray header(value.data(), value.size());
+        (useTrailers ? trailers : headers)
+            .insert(QByteArray(name.data(), name.size()),
+                    std::move(header));
+    }
+
+    void on_header_impl(boost::system::error_code&)
+    {
+        auto it = headers.find("connection");
+        bool close_found = false;
+        bool keep_alive_found = false;
+        for (;it != headers.end();++it) {
+            auto value = boost::string_ref(it->data(), it->size());
+            boost::http::header_value_any_of(value, [&](boost::string_ref v) {
+                    if (iequals(v, "close"))
+                        close_found = true;
+
+                    if (iequals(v, "keep-alive"))
+                        keep_alive_found = true;
+
+                    if (iequals(v, "upgrade"))
+                        is_upgrade = true;
+
+                    return false;
+                });
+            if (close_found)
+                break;
+        }
+        if (!close_found
+            && (httpVersion == HttpVersion::HTTP_1_1 || keep_alive_found)) {
+            responseOptions |= HttpServerResponse::KEEP_ALIVE;
+        }
+        whatEmit = Priv::READY;
+        useTrailers = true;
+    }
+
+    void on_body_init_impl(const boost::optional<std::uint64_t>&,
+                           boost::system::error_code&)
+    {}
+
+    std::size_t on_body_impl(boost::string_view s,
+                             boost::system::error_code&)
+    {
+        on_body_or_chunk(s);
+        return s.size();
+    }
+
+    void on_chunk_header_impl(std::uint64_t, boost::string_view,
+                              boost::system::error_code&)
+    {}
+
+    std::size_t on_chunk_body_impl(std::uint64_t,
+                                   boost::string_view body,
+                                   boost::system::error_code&)
+    {
+        on_body_or_chunk(body);
+        return body.size();
+    }
+
+    void on_body_or_chunk(boost::string_view value)
+    {
+        body.append(value.data(), value.size());
+        whatEmit |= Priv::DATA;
+    }
+
+    void on_finish_impl(boost::system::error_code&)
+    {
+        useTrailers = false;
+        whatEmit |= Priv::END;
+        disconnect(&socket, SIGNAL(readyRead()), request, SLOT(onReadyRead()));
+    }
+
+    Tufao::HttpServerRequest *request;
     QAbstractSocket &socket;
     QByteArray buffer;
-    reader::request parser;
-    QByteArray lastHeader;
+    Signals whatEmit;
+    bool is_upgrade;
     bool useTrailers;
     QByteArray body;
 
